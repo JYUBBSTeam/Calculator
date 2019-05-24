4452fb2f9b94fccb7bbfeb364029f6f469b39ed8
h <JYUBBSTeam@outlook.com>
2019-05-24 10:17:29 +0800

    2019.5.24_huang

--- ComputingLibrary/test/testElementaryFunction/cmake-build-debug/cmake_install.cmake
+++ ComputingLibrary/test/testElementaryFunction/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /cygdrive/e/project/GIT/Calculator/ComputingLibrary/test/testElementaryFunction
+# Install script for directory: /home/h/Project/Calculator/ComputingLibrary/test/testElementaryFunction
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -27,6 +27,11 @@ if(NOT CMAKE_INSTALL_COMPONENT)
   endif()
 endif()
 
+# Install shared libraries without execute permission?
+if(NOT DEFINED CMAKE_INSTALL_SO_NO_EXE)
+  set(CMAKE_INSTALL_SO_NO_EXE "1")
+endif()
+
 # Is this installation the result of a crosscompile?
 if(NOT DEFINED CMAKE_CROSSCOMPILING)
   set(CMAKE_CROSSCOMPILING "FALSE")
@@ -40,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/cygdrive/e/project/GIT/Calculator/ComputingLibrary/test/testElementaryFunction/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/h/Project/Calculator/ComputingLibrary/test/testElementaryFunction/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")

cd6db3b3611be8aebf136646b3a0129e4ff28c10
JYUBBSTeam <jyubbsteam@outlook.com>
2019-09-04 20:16:17 +0800

    2019_9_4_huang

--- Calculator/Window/runMainWindow.py
+++ Calculator/Window/runMainWindow.py
@@ -1,37 +1,39 @@
-'''
+"""
     创建人：Liang
     创建时间：2019/7/18
     最后一次编辑时间：
     描述：主窗口(创建菜单栏、状态栏、工具栏）
     类: mainWindow:主窗口
-'''
+"""
 
 import sys
+#import time    load_Message依赖此module，考虑删除
 
 from PyQt5.QtWidgets import QApplication, qApp
 from Calculator.Window.mainWindow.mainWindow import mainWindow
 from Calculator.Window.commomHelper_loadQss.commomHelper_Qss import CommonHelper_qss
 from Calculator.Window.mainWindow.init_Splash import initSplash
 
-import time
-
-
 # 启动界面显示时间的设置
+"""
 def load_Message(splash):
-    for i in range (1, 5):  # 显示时间4秒
-        time.sleep(0.5)   # 睡眠
-        splash.setText("初始化程序...{0}%".format(25*i))
+    for i in range(1, 5):  # 显示时间4秒
+        time.sleep(0.5)  # 睡眠
+        splash.setText("初始化程序...{0}%".format(25 * i))
         splash.update()
         qApp.processEvents()
+"""
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
 
+    """ 取消启动界面
     splash = initSplash()
     splash.show()
     qApp.processEvents()
     load_Message(splash)
     splash.close()  # 隐藏启动界面
+    """
 
     main_Window = mainWindow()
 
